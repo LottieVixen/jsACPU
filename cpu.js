d0581f596c192baad4a9839c058d4a3f675cdcbd
Charlotte Fields <charlottelilyfields48@gmail.com>
2017-06-05 03:57:11 +1000

    added stuff

--- cpu.js
+++ cpu.js
@@ -1,18 +1,33 @@
 //Generate memory array buffer, 8*0xff
-var memspace 	= new ArrayBuffer(0xff); //make arraybuffer (bytes) in size
-var mem 		= Uint8Array(memspace); //make array split into 8bit chunks
+var memspace 	//= new ArrayBuffer(0xff); //make arraybuffer (bytes) in size
+var mem 		//= Uint8Array(memspace); //make array split into 8bit chunks
 
 //make CPU class
 class CPU {
 	constructor(){
+		this.start = 0x00;
+	}
+	halt(){}
+	execute(instr){
+		var A = mem[instr];
+		var B = mem[instr+1];
+		var C = mem[instr+2];
+		if (!(A+B)){
+			if (C==(instr+2)){ //if C points to it's own position
+				halt();
+			}
+		}
 	}
 	run() {
-		let instruction = mem[0x00] 
+		let instruction = mem[this.start];
+		execute(instruction);
 	}
 }
 
 fetch("./nop.bin")
 	.then((response) => response.arrayBuffer())
 	.then((arrayBuffer) => {
-		memspace = arrayBuffer;
+		mem = Uint8Array(arrayBuffer);
+		let cpu = new CPU();
+		cpu.run();
 	})
